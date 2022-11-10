06e15a34c98d00f2ba9936f52189d63043f74142
Rishi Varma Kalidindi <rishivarma.lc.dev@gmail.com>
2022-11-09 19:16:58 -0500

    Project Done

---
+++ RoseBlanches/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    static int recievedAmount, remainingAmount, restOfTheAmount;
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        input();
+        calcRemainAmount();
+        calcCanBuy();
+    }
+    public static void input() {
+        System.out.print("\n  How much money did you recieve ? :  $");
+        recievedAmount = scan.nextInt();
+    }
+    public static void calcRemainAmount() {
+        remainingAmount = recievedAmount - ((recievedAmount/4)*3);
+        System.out.println("\n  Book and Supplies :  $" + ((recievedAmount/4)*3));
+    }
+    public static void calcCanBuy() {
+        final int cafePrice = 2;
+        final int flashCompPrice = 4;
+        final int subwayTicketPrice = 3;
+        int perUnitTotalPrice = remainingAmount/3;
+        System.out.println("  You Can Buy :");
+        System.out.println("    " + (perUnitTotalPrice/cafePrice) + " coffees");
+        System.out.println("    " + (perUnitTotalPrice/flashCompPrice) + " Flash Computer Numbers");
+        System.out.println("    " + (perUnitTotalPrice/subwayTicketPrice) + " Subway Tickets");
+        
+    }
+}
